8d7ac41177b0c924ee26f7d1ca710686081e2ac1
S.Snavely <info@stevesnavely.com>
2022-04-22 23:19:47 -0400

    Updated Code

--- index.js
+++ index.js
@@ -4,34 +4,38 @@ const fs = require("fs");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
-const teamArr = [];
+const teamArr = [];//array to hold newly created employees
 
-const generateHTML = (answers) => {
-  `<!DOCTYPE html>
+const generateHTML = (answers) => {//function to create html to populate on dist/index.html file.
+
+  return `<!DOCTYPE html>
   <html lang="en">
   <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="./dist/style.css">
+    <title>My Team</title>
   </head>
   <body>
-    <div class="jumbotron jumbotron-fluid">
+  <header>  
+   
+  <div class="jumbotron jumbotron-fluid">
     <div class="container">
-      <h1 class="display-4"></h1>
-      <p class="lead">I am from</p>
-      <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-      <ul class="list-group">
-        <li class="list-group-item">My GitHub username is </li>
-        <li class="list-group-item">My Food username is </li>
-        <li class="list-group-item">LinkedIn:</li>
-      </ul>
+      <h1 class="display-4">My Team</h1>
     </div>
   </div>
+  </header> 
+<main class="container ">
+<div> Employee Cards go here
+</div>
+</main>
+
   </body>
   </html>`;
-  
-  inquirer
+};
+
+  inquirer // creates object for Manager
     .prompt([
       {
         type: "input",
@@ -54,31 +58,31 @@ const generateHTML = (answers) => {
         message: "What is your team manager's office number?",
       },
       {
-        type: "list",
+        type: "list",// list to choose which type of employee to create
         name: "eType",
         message: "Which type of team member would you like to add?",
         choices: ["Engineer", "Intern", "Done"],
       },
     ])
     .then((mAnswers) => {
-      const newManager = new Manager(
+      const newManager = new Manager(//creates the manager object 
         mAnswers.mName,
         mAnswers.mId,
         mAnswers.mEmail,
         mAnswers.officeNum
       );
-      teamArr.push(newManager);
-      if (mAnswers.eType === "Engineer") {
+      teamArr.push(newManager);//pushes the user input into the array
+      if (mAnswers.eType === "Engineer") { // if the engineer from the list is choosen then it is sent create an engineer 
         eQuestion();
-      } else if (mAnswers.eType === "Intern") {
+      } else if (mAnswers.eType === "Intern") {// if the intern from the list is choosen then it is sent to the Intern 
         iQuestion();
-      } else if (mAnswers.eType === "Done") {
+      } else if (mAnswers.eType === "Done") { //user is finished creating a team and the html is created by calling the function addTeam
         addTeam();
        
       }
     });
-
-  const eQuestion = (eAnswers) => {
+  
+  const eQuestion = (eAnswers) => {// this the creation of the engineer object/ repeats the manager  
     inquirer
       .prompt([
         {
@@ -126,7 +130,7 @@ const generateHTML = (answers) => {
       });
   };
 
-  function iQuestion(iAnswers) {
+  function iQuestion(iAnswers) {// this the creation of the intern object/ repeats the manager 
     inquirer
       .prompt([
         {
@@ -157,7 +161,12 @@ const generateHTML = (answers) => {
         },
       ])
       .then((iAnswers) => {
-        const newIntern = new Intern();
+        const newIntern = new Intern( 
+          iAnswers.iName,
+          iAnswers.iId,
+          iAnswers.iEmail,
+          iAnswers.school
+          );
         teamArr.push(newIntern);
         if (iAnswers.addType === "Engineer") {
           iQuestion(iAnswers);
@@ -169,12 +178,9 @@ const generateHTML = (answers) => {
         }
       });
   }
-};
-const addTeam = () => {
-  // const htmlPageContent = generateHTML(answers);
-  fs.writeFile("./dist/index.html", (teamArr), "utf-8", (err) =>
+const addTeam = () => {//the addTeam functions generates the html to the index.html file.
+  fs.writeFile("./dist/index.html", generateHTML (teamArr), "utf-8", (err) =>
     err ? console.log(err) : console.log("Successfully created index.html!")
   );
 };
 
-generateHTML();
